6235a5e200689215ba535212fe7939858a66f106
Kalfian <hi@kalfian.com>
2023-01-19 20:21:16 +0700

    Detail Appointment + Detail Activity

---
+++ app/src/main/java/com/kalfian/stiki/stiki_e_appointment/modules/student/DetailAppointmentActivity.kt
@@ -0,0 +1,12 @@
+package com.kalfian.stiki.stiki_e_appointment.modules.student
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kalfian.stiki.stiki_e_appointment.R
+
+class DetailAppointmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_appointment)
+    }
+}
